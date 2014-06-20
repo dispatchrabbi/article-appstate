3eb385e2133dff6e90a4d24d318591f80905c758
Brian Moschel <brian.moschel@gmail.com>
2014-06-20 12:59:13 -0500

    remove console.log

--- appstate/index.js
+++ appstate/index.js
@@ -8,10 +8,6 @@ steal(
 function(can, AppState, appTemplate) {
 	$(document).ready(function(){
 		window.appState = new AppState();
-		appState.bind('searchTerm', function(){
-debugger;
-			console.log("changed search")
-		})
 		can.route.map(appState);
 		can.route.ready();
 		$("#app").html(appTemplate(appState));
